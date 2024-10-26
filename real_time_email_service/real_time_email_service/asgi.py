186025d7eb6b3cc377c720a241b9dcc5c318ddaa
Neto <joseneto55peixoto@gmail.com>
2024-10-25 22:32:48 -0300

    Adicionando asgi para suporte de sincronizalçao, suporte para a comunicação em tempo real

--- real_time_email_service/real_time_email_service/asgi.py
+++ real_time_email_service/real_time_email_service/asgi.py
@@ -1,16 +1,18 @@
-"""
-ASGI config for real_time_email_service project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from messaging import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_time_email_service.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    ),
+})
+
