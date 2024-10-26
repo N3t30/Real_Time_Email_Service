c6d1833bd8f84ce148a507719753ea547b4ee084
Neto <joseneto55peixoto@gmail.com>
2024-10-25 23:25:49 -0300

    Adicionando as rotas da API para enviar e receber mensagens

--- real_time_email_service/messaging/views.py
+++ real_time_email_service/messaging/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Message
+from .serializers import MessageSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+class SendMessageView(generics.CreateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class InboxView(generics.ListAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Message.objects.filter(recipient=self.request.user)
+
+class OutboxView(generics.ListAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Message.objects.filter(sender=self.request.user)
+
